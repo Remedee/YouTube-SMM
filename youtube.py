1d720e304b50c34256cde8ad66653f250af6968f
Remedee <zacharydanieldowns@gmail.com>
2019-04-10 16:22:00 -0500

    Collection Begins

--- youtube.py
+++ youtube.py
@@ -4,6 +4,10 @@
 from apiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 
+# List of popular videos and their ids.
+popular=[]
+popids=[]
+
 # Get credentials from client secret.
 CLIENT_SECRET_FILE = 'client_secret.json'
 SCOPES = ['https://www.googleapis.com/auth/youtube']
@@ -18,10 +22,32 @@ youtube = build('youtube', 'v3', developerKey=apiKey, credentials=credentials)
 request = youtube.videos().list(part='snippet', chart="mostPopular", maxResults="5", regionCode="US")
 result = request.execute()
 
-# Request the related videos of the relatedToVideoID video and run that request.
-request = youtube.search().list(part='snippet', relatedToVideoId='niz3Shv6QWY', type='video')
+#  Get a list of the most popular video titles.
+for item in result['items']:
+    popular.append(item['snippet']['title'])
+
+# Get the related video ids of the 5 most popular youtube titles.
+request = youtube.videos().list(part='id', chart="mostPopular", maxResults="5", regionCode="US")
 result = request.execute()
 
-# Print out the titles of related videos.
+# Store these ids into popids.
 for item in result['items']:
-    print(item['snippet']['title'])
+    popids.append(item['id'])
+
+# Loop to output the titles of popular youtube videos and their related videos.
+for video in range(len(popids)):
+
+    # Request the related videos of the relatedToVideoID video and run that request.
+    request = youtube.search().list(part='snippet', relatedToVideoId=popids[video], type='video')
+    result = request.execute()
+
+    # Print out the popular video id title.
+    print('')
+    print(popular[video])
+    print('')
+
+    # Print out the titles of related videos.
+    for item in result['items']:
+        print(item['snippet']['title'])
+
+    print('')
