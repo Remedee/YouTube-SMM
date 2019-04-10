1d8aba5a19476e8c7089312de7cc7264de458d35
Remedee <zacharydanieldowns@gmail.com>
2019-04-10 13:36:52 -0500

    Documentation + Bug Fix
    Added more documentation as well as fixed a bug with IAF needing to be called InstalledAppFlow.

--- youtube.py
+++ youtube.py
@@ -1,18 +1,23 @@
 # Zachary Edwards Downs
-from apiclient.discovery import build
-from google_auth_oauthlib.flow import IAF
 
+# Necessary imports.
+from apiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# Get credentials from client secret.
 CLIENT_SECRET_FILE = 'client_secret.json'
 SCOPES = ['https://www.googleapis.com/auth/youtube']
-flow = IAF.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
 credentials = flow.run_console()
 
+# Build the api.
 apiKey = 'AIzaSyBDfBHkXmijo0Ji4Lv2pae95dJ1dyxwTo0'
 youtube = build('youtube', 'v3', developerKey=apiKey, credentials=credentials)
  
+# Request the related videos of the relatedToVideoID video and run that request.
 request = youtube.search().list(part='snippet', relatedToVideoId='niz3Shv6QWY', type='video')
-
 result = request.execute()
-#print(result)
-#for item in result['items']:
-#    print(item['snippet']['title'])
+
+# Print out the titles of related videos.
+for item in result['items']:
+    print(item['snippet']['title'])
