a15aa17c1b5935dbc16d5f0e21517c92d124c738
Remedee <zacharydanieldowns@gmail.com>
2019-04-10 15:50:19 -0500

    Request for Most Popular

--- youtube.py
+++ youtube.py
@@ -13,7 +13,11 @@ credentials = flow.run_console()
 # Build the api.
 apiKey = 'AIzaSyBDfBHkXmijo0Ji4Lv2pae95dJ1dyxwTo0'
 youtube = build('youtube', 'v3', developerKey=apiKey, credentials=credentials)
- 
+
+# Grabs 5 videos from YouTube's most popular in the US.
+request = youtube.videos().list(part='snippet', chart="mostPopular", maxResults="5", regionCode="US")
+result = request.execute()
+
 # Request the related videos of the relatedToVideoID video and run that request.
 request = youtube.search().list(part='snippet', relatedToVideoId='niz3Shv6QWY', type='video')
 result = request.execute()
