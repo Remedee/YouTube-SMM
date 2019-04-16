8524cee70361dc3b74337865555c33994085d980
Remedee <zacharydanieldowns@gmail.com>
2019-04-16 13:48:24 -0500

    Grab the 50 Most Popular
    Complete the script to grab the 50 most popular YouTube videos at the time the script is run and 15 of their related videos. This is all done with video IDS as title can become quite lengthy.

--- youtube.py
+++ youtube.py
@@ -2,13 +2,13 @@
 
 # Necessary imports.
 import csv
+import os
 from apiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-# List of popular videos and their ids.
-popular=[]
+# List of popular videos ids and data file names.
 popids=[]
-array=[]
+fnames=[]
 
 # Get credentials from client secret.
 CLIENT_SECRET_FILE = 'client_secret.json'
@@ -20,51 +20,64 @@ credentials = flow.run_console()
 apiKey = 'AIzaSyBDfBHkXmijo0Ji4Lv2pae95dJ1dyxwTo0'
 youtube = build('youtube', 'v3', developerKey=apiKey, credentials=credentials)
 
-# Grabs 5 videos from YouTube's most popular in the US.
-request = youtube.videos().list(part='snippet', chart="mostPopular", maxResults="5", regionCode="US")
-result = request.execute()
-
-#  Get a list of the most popular video titles.
-for item in result['items']:
-    popular.append(item['snippet']['title'])
-
 # Get the related video ids of the 5 most popular youtube titles.
-request = youtube.videos().list(part='id', chart="mostPopular", maxResults="5", regionCode="US")
+request = youtube.videos().list(part='id', chart="mostPopular", maxResults="50", regionCode="US")
 result = request.execute()
 
 # Store these ids into popids.
 for item in result['items']:
     popids.append(item['id'])
 
+# Change to the data directory.
+os.chdir('data')
 
-with open('text.csv', mode='w') as employee_file:
-    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+# Get the file names in the data directory.
+for files in os.walk("."):
+        for file in files:
+            if '.' not in file:
+                fnames = file
 
-    # Loop to output the titles of popular youtube videos and their related videos.
+# Sort the file names alphabetically.
+fnames.sort()
+
+# Generate an unused filename.
+for i in range(len(fnames) + 1):
+
+    # If all files have been checked generate a name then break.
+    if i == len(fnames):
+        name = 'data' + str(i) + '.csv'
+        break
+
+    name = 'data' + str(i) + '.csv' # Generate a file name.
+
+    # If the generated file name does not exist then use it and break.
+    if name != fnames[i]:
+        break
+
+# Open a csv file to write the data to.
+with open(name, mode='w') as youtubeIDS:
+    youtube_writer = csv.writer(youtubeIDS, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    # Loop to append popular videoIds and their related videoIds to a csv.
     for video in range(len(popids)):
 
-        test=[]
-        array=[]
+        data=[]
+        related=[]
 
         # Request the related videos of the relatedToVideoID video and run that request.
-        request = youtube.search().list(part='id', relatedToVideoId=popids[video], type='video')
+        request = youtube.search().list(part='id', relatedToVideoId=popids[video], type='video', maxResults='15')
         result = request.execute()
 
-        # Print out the popular video id title.
-        print('')
-        print(popular[video])
-        print('')
-
-        # Print out the titles of related videos.
+        # Append videoId's to the related array.
         for item in result['items']:
-            print(item['id']['videoId'])
-            array.append(item['id']['videoId'])
+            related.append(item['id']['videoId'])
 
-        test.append(popids[video])
+        # Append popular videoID to the data array.
+        data.append(popids[video])
 
-        for i in range(len(array)):
-            test.append(array[i])
+        # Append related videos to the data array, after the popular videoID.
+        for i in range(len(related)):
+            data.append(related[i])
 
-        employee_writer.writerow(test)
-
-        print('')
+        # Store the data array as a line in a csv file.
+        youtube_writer.writerow(data)
